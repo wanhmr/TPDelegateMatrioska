a4dfce07bdc8fa2211179dc4555ca624cd5391ff
Tpphha <tpphha@gmail.com>
2017-07-22 12:48:50 +0800

    initial podspec

---
+++ TPDelegateMatrioskaDemo/TPDelegateMatrioskaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TPDelegateMatrioskaDemo
+//
+//  Created by Tpphha on 2017/7/22.
+//  Copyright © 2017年 Tpphha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
